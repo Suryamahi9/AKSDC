a84b3495250017384ab019eb83e04a1c6938b1c2
Mahendra Surya <149085651+Suryamahi9@users.noreply.github.com>
2025-01-10 23:47:44 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-prod"                  # The resource group where the storage account is located
-    storage_account_name  = "tfstatestorageacct"        # The name of the storage account
+    storage_account_name  = "tfstatestorageacct11"        # The name of the storage account
     container_name        = "terraform-state"           # The name of the container to store the state file
     key                   = "terraform.tfstate"         # The name of the state file within the container
   }
